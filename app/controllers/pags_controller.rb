d19e4d8240356d1c9c4e5960aa3eeba11948f3a6
Joaquin Grez <joaquingrez@gmail.com>
2017-06-07 20:29:06 -0300

    Last commit

--- app/controllers/pags_controller.rb
+++ app/controllers/pags_controller.rb
@@ -1,7 +1,21 @@
 class PagsController < ApplicationController
+  
   def pag1
+  	render layout: "layout"
   end
 
   def pag2
+
   end
+
+  def pag3
+
+  end
+
+  private
+
+  def get_data
+    @users = ["Rosario Valech", "Enyel Muñoz", "Joaquin Grez"]
+  end
+
 end
